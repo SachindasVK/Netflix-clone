b84cab0e5c0873696a1f5338fbbf063b0bc906e4
sachin <sachinvk867@gmail.com>
2026-04-20 03:09:34 +0530

    changes added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,23 @@
 import Navbar from "../../components/Navbar/Navbar";
 import LandFooter from "@/components/LandFooter/LandFooter";
 import React from "react";
-import Hero from '../../components/Hero/Hero'
+import Hero from "../../components/Hero/Hero";
 import MovieCards from "@/components/MovieCards/MovieCards";
 const HomeCompo = () => {
   return (
     <>
       <Navbar />
       <div className="home">
-          <Hero />
+        <Hero />
         <div>
           <MovieCards
             type={"movie"}
             category={"now_playing"}
             title={"Your Next Watch"}
           />
-
-          <MovieCards
-            type={"movie"}
-            category={"top_rated"}
-            title={"Top Rated"}
-          />
           <MovieCards type={"movie"} category={"upcoming"} title={"Upcoming"} />
+
+        
           <MovieCards
             type={"tv"}
             category={"top_rated"}
